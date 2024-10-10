d8d6e14a0bc4ebdaa84793b77ba5c5d6ca6e98c7
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2024-10-10 21:31:59 +0530

    updation of the marquee page

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -11,7 +11,7 @@ import React from 'react'
 function Marquee() {
   return (
     <div className='w-full bg-[#004D43] rounded-t-[1.5vw] overflow-hidden'>
-        <div className='line border-y-2 h-[40vh] border-[#306F66] mt-24 text-[30vh] flex items-center mb-10 whitespace-nowrap'>
+        <div className='line border-y-2 h-[40vh] border-[#306F66] mt-24 text-[35vh] flex items-center mb-10 whitespace-nowrap font-md font-["Neue_Montreal"]'>
             <h1>WE ARE OCHI</h1>
             <h1>WE ARE OCHI</h1>
             <h1>WE ARE OCHI</h1>
