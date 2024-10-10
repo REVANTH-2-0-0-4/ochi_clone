07d9c0ff432033d7a2bd5d1109ea6c088a9a760f
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2024-10-10 21:14:27 +0530

    completion of the marquee page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Landingpage from './components/Landingpage'
+import Marquee from './components/Marquee'
 function App() {
   return (
-<div className="w-full h-screen  text-white ">
+<div className="w-full min-h-screen  text-white  bg-zinc-900 ">
 
 
      <Navbar></Navbar>
      <Landingpage/>
+     <Marquee></Marquee>
+     {/* <Marquee></Marquee> */}
+     
 </div>
   )
 } 
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+
+
+
+
+
+
+function Marquee() {
+  return (
+    <div className='w-full bg-[#004D43] rounded-t-[1.5vw] overflow-hidden'>
+        <div className='line border-y-2 h-[40vh] border-[#306F66] mt-24 text-[30vh] flex items-center mb-10 whitespace-nowrap'>
+            <h1>WE ARE OCHI</h1>
+            <h1>WE ARE OCHI</h1>
+            <h1>WE ARE OCHI</h1>
+        </div>
+     
+    </div>
+  )
+}
+
+export default Marquee
