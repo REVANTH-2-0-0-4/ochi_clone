e3ec9853b02a6d5da00c3c5e0721fedb59d9a2c5
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2024-10-16 16:06:04 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Landingpage from './components/Landingpage'
 import Marquee from './components/Marquee'
 import About from './components/About'
+import Eyes from './components/Eyes'
 function App() {
   return (
 <div className="w-full min-h-screen  text-white  bg-zinc-900 ">
@@ -12,6 +13,7 @@ function App() {
      <Landingpage/>
      <Marquee></Marquee>
       <About></About>     
+      <Eyes></Eyes>
 </div>
   )
 } 
---
+++ src/components/Eyes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Eyes() {
+  return (
+    <div className='bg-white text-black h-screen w-full overflow-hidden '>
+      eyes
+    </div>
+  )
+}
+
+export default Eyes
